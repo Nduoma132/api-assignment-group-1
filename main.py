ec49721efe322946612d644c11f4854d19c45f6d
Nduoma132 <nduoma132@gmail.com>
2025-05-15 12:51:23 +0100

    Third commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import random
-
+from model import *
+from services import *
 app = FastAPI()
 
 books = [
@@ -22,83 +23,82 @@ books = [
 
 
 #ALL FUNCTIONS
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
 
-def get_all_books():
-    return books
+# def get_all_books():
+#     return books
 
-def get_book_by_title(book_title):
-    new_books = []
-    for book in books:
-        if book_title.lower() == book["title"].lower():
-            new_books.append(book)
-    return new_books
+# def get_book_by_title(book_title):
+#     new_books = []
+#     for book in books:
+#         if book_title.lower() == book["title"].lower():
+#             new_books.append(book)
+#     return new_books
 
-def get_book_by_author(author_name):
-    new_books = []
-    for book in books:
-        if author_name.lower() == book["author"].lower():
-            new_books.append(book)
-    return new_books
+# def get_book_by_author(author_name):
+#     new_books = []
+#     for book in books:
+#         if author_name.lower() == book["author"].lower():
+#             new_books.append(book)
+#     return new_books
 
 
-def add_new_book_to_list(title: str, author: str):
-    for existing_book in books:
-        if existing_book["title"].lower() == title.lower() and existing_book["author"].lower() == author.lower():
-            return "Error: This book already exists."
+# def add_new_book_to_list(title: str, author: str):
+#     for existing_book in books:
+#         if existing_book["title"].lower() == title.lower() and existing_book["author"].lower() == author.lower():
+#             return "Error: This book already exists."
 
-    books.append({"title": title, "author": author})
-    return f"Success: {title} has been added"
+#     books.append({"title": title, "author": author})
+#     return f"Success: {title} has been added"
 
-def update_book_by_title(current_title: str, new_title: str = None, new_author: str = None):
-    for book in books:
-        if book["title"].lower() == current_title.lower():
-            if new_title:
-                book["title"] = new_title
-            if new_author:
-                book["author"] = new_author
-            return {"message": "Book updated successfully"}
-    return {"error": "Book not found"}
+# def update_book_by_title(current_title: str, new_title: str = None, new_author: str = None):
+#     for book in books:
+#         if book["title"].lower() == current_title.lower():
+#             if new_title:
+#                 book["title"] = new_title
+#             if new_author:
+#                 book["author"] = new_author
+#             return {"message": "Book updated successfully"}
+#     return {"error": "Book not found"}
 
-def get_book_by_prime():
-    new_books = []
-    for book in books:
-        # split_val = book["title"][-1]
-        split_title = book["title"].split()
-        if len(split_title) >= 2 and split_title[1].isdigit():
-            split_val = int(split_title[1])
-            if is_prime(split_val):
-                new_books.append(book)
-    return new_books
+# def get_book_by_prime():
+#     new_books = []
+#     for book in books:
+#         # split_val = book["title"][-1]
+#         split_title = book["title"].split()
+#         if len(split_title) >= 2 and split_title[1].isdigit():
+#             split_val = int(split_title[1])
+#             if is_prime(split_val):
+#                 new_books.append(book)
+#     return new_books
 
-def delete_book_by_title(book_title):
-    for book in books:
-        if book_title.lower() == book["title"].lower():
-            books.remove(book)
-    return f"Success: {book['title']} has been removed"
+# def delete_book_by_title(book_title):
+#     for book in books:
+#         if book_title.lower() == book["title"].lower():
+#             books.remove(book)
+#     return f"Success: {book['title']} has been removed"
 
-def find_empty_authors(book_title):
-    for book in books:
-        if book_title.lower() == book["title"].lower() and book["author"] == "":
-            return f"{book['title']} has an empty author"
-    return "This book has an author"
+# def find_empty_authors(book_title):
+#     for book in books:
+#         if book_title.lower() == book["title"].lower() and book["author"] == "":
+#             return f"{book['title']} has an empty author"
+#     return "This book has an author"
 
-def search_by_keyword(the_keyword):
-    search_results = []
-    for book in books:
-        if the_keyword.lower() in book["title"].lower() or (book["author"] == "" and the_keyword.lower() in book["title"].lower()):
-            search_results.append({"title": book["title"], "author": book["author"]})
-    if search_results:
-        return search_results
-    else:
-        return "No matches in the database"
-
+# def search_by_keyword(the_keyword):
+#     search_results = []
+#     for book in books:
+#         if the_keyword.lower() in book["title"].lower() or (book["author"] == "" and the_keyword.lower() in book["title"].lower()):
+#             search_results.append({"title": book["title"], "author": book["author"]})
+#     if search_results:
+#         return search_results
+#     else:
+#         return "No matches in the database"
 
 
 #ALL ENDPOINTS
